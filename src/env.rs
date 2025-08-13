eb9f73fb1c5e1dc185d056b13bd8a836ef95d4e2
Mason <Mordinel@pm.me>
2025-08-14 07:08:22 +1200

    Move all this shit to macros much nicer now.

--- src/env.rs
+++ src/env.rs
@@ -1,31 +1,29 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::{Add, Div, Mul, Sub}};
 
 use crate::{error::Error, expr::Expr, number::Num};
 
-macro_rules! ensure_tonicity {
-    ($check_fn:expr) => {
-        |args: &[Expr]| -> Result<Expr, Error> {
-            let numbers = parse_list_of_numbers(args)?;
-            let first = numbers
-                .first()
-                .ok_or(Error::Reason("Expected at least one number".to_string()))?;
+macro_rules! comparative_binary_func {
+    ($fname:expr, $check_fn:expr, $data:expr) => {
+        $data.insert(
+            $fname.to_string(), 
+            Expr::Func($fname.to_string(), |args: &[Expr]| -> Result<Expr, Error> {
+                let numbers = parse_list_of_numbers(args)?;
+                let first = numbers.first()
+                    .ok_or_else(|| Error::Reason(format!("({} n n ...): Missing first number.", $fname)))?;
+                let rest = &numbers.get(1..)
+                    .ok_or_else(|| Error::Reason(format!("({} n n ...): Missing second number.", $fname)))?;
 
-            let rest = &numbers[1..];
-
-            fn trunk(prev: &Num, rest: &[Num]) -> bool {
-                match rest.first() {
-                    Some(fl) => $check_fn(prev, fl) && trunk(fl, &rest[1..]),
-                    None => true,
+                fn trunk(prev: &Num, rest: &[Num]) -> bool {
+                    match rest.first() {
+                        Some(fl) => $check_fn(prev, fl) && trunk(fl, &rest[1..]),
+                        None => true,
+                    }
                 }
-            }
-            Ok(Expr::Bool(trunk(first, rest)))
-        }
+                Ok(Expr::Bool(trunk(first, rest)))
+            }),
+        );
     };
 }
-    //data.insert(
-    //    "=".to_string(),
-    //    Expr::Func("=".into(), ensure_tonicity!(|a, b| a == b)),
-    //);
 
 macro_rules! numeric_binary_func {
     ($fname:expr, $fold_closure:expr, $data:expr) => {
@@ -33,15 +31,10 @@ macro_rules! numeric_binary_func {
             $fname.to_string(),
             Expr::Func($fname.to_string(), |args: &[Expr]| -> Result<Expr, Error> {
                 let numbers = parse_list_of_numbers(args)?;
-                let first = numbers.first().ok_or_else(|| {
-                    Error::Reason(format!("({} n n ...): Missing first number.", $fname))
-                })?;
-                let folded = numbers
-                    .get(1..)
-                    .ok_or_else(|| {
-                        Error::Reason(format!("({} n n ...): Missing second number.", $fname))
-                    })?
-                .iter()
+                let first = numbers.first()
+                    .ok_or_else(|| Error::Reason(format!("({} n n ...): Missing first number.", $fname)))?;
+                let folded = numbers.get(1..)
+                    .ok_or_else(|| Error::Reason(format!("({} n n ...): Missing second number.", $fname)))?.iter()
                     .fold(first.clone(), $fold_closure);
                 Ok(Expr::Number(folded))
             }),
@@ -56,7 +49,7 @@ macro_rules! numeric_unary_func {
             Expr::Func($fname.to_string(), |args: &[Expr]| -> Result<Expr, Error> {
                 let numbers = parse_list_of_numbers(args)?;
                 let first = numbers.first()
-                    .ok_or_else(|| Error::Reason(format!("({} n):Missing first number.", $fname)))?;
+                    .ok_or_else(|| Error::Reason(format!("({} n): Missing first number.", $fname)))?;
                 if args.len() > 1 {
                     return Error::Reason(format!("({} n): Can only have one argument.", $fname)).into();
                 }
@@ -65,6 +58,21 @@ macro_rules! numeric_unary_func {
         );
     };
 }
+
+macro_rules! numeric_constant_func {
+    ($fname:expr, $func:expr, $data:expr) => {
+        $data.insert(
+            $fname.to_string(),
+            Expr::Func($fname.to_string(), |args: &[Expr]| -> Result<Expr, Error> {
+                if !args.is_empty() {
+                    return Error::Reason(format!("({}): No arguments allowed in a constant function.", $fname)).into();
+                }
+                Ok(Expr::Number($func()))
+            })
+        );
+    };
+}
+
 
 #[derive(Debug)]
 pub struct Env<'outer> {
@@ -78,8 +86,7 @@ pub fn default_env<'outer>() -> Env<'outer> {
     data.insert(
         "print".to_string(),
         Expr::Func("print".into(), |args| {
-            let first = args
-                .first()
+            let first = args.first()
                 .ok_or_else(|| Error::Reason("print: Expected at least one form.".to_string()))?;
             if args.len() > 1 {
                 return Error::Reason("print: can only have one form.".to_string()).into();
@@ -92,50 +99,38 @@ pub fn default_env<'outer>() -> Env<'outer> {
     data.insert(
         "println".to_string(),
         Expr::Func("println".into(), |args| {
-            let first = args
-                .first()
-                .ok_or_else(|| Error::Reason("print: Expected at least one form.".to_string()))?;
+            let first = args.first()
+                .ok_or_else(|| Error::Reason("(println n): Missing first argument.".to_string()))?;
             if args.len() > 1 {
-                return Error::Reason("print: can only have one form.".to_string()).into();
+                return Error::Reason("(println n): Can only have one argument.".to_string()).into();
             }
             println!("{first}");
             Ok(Expr::Nil)
         }),
     );
 
-    data.insert(
-        "=".to_string(),
-        Expr::Func("=".into(), ensure_tonicity!(|a, b| a == b)),
-    );
-    data.insert(
-        ">".to_string(),
-        Expr::Func(">".into(), ensure_tonicity!(|a, b| a > b)),
-    );
-    data.insert(
-        ">=".to_string(),
-        Expr::Func(">=".into(), ensure_tonicity!(|a, b| a >= b)),
-    );
-    data.insert(
-        "<".to_string(),
-        Expr::Func("<".into(), ensure_tonicity!(|a, b| a < b)),
-    );
-    data.insert(
-        "<=".to_string(),
-        Expr::Func("<=".into(), ensure_tonicity!(|a, b| a <= b)),
-    );
+    comparative_binary_func!( "=", |a, b| a == b, data);
+    comparative_binary_func!( ">", |a, b| a >  b, data);
+    comparative_binary_func!(">=", |a, b| a >= b, data);
+    comparative_binary_func!( "<", |a, b| a <  b, data);
+    comparative_binary_func!("<=", |a, b| a <= b, data);
 
-    numeric_binary_func!(    "+", |l, r| l + r.clone(),    data);
-    numeric_binary_func!(    "-", |l, r| l - r.clone(),    data);
-    numeric_binary_func!(    "*", |l, r| l * r.clone(),    data);
-    numeric_binary_func!(    "/", |l, r| l / r.clone(),    data);
-    numeric_binary_func!(    "^", |l, r| l.pow(r),         data);
-    numeric_binary_func!(  "log", |l, r| l.log(r.clone()), data);
+    numeric_binary_func!(  "+", |l, r| l.add(r.clone()), data);
+    numeric_binary_func!(  "-", |l, r| l.sub(r.clone()), data);
+    numeric_binary_func!(  "*", |l, r| l.mul(r.clone()), data);
+    numeric_binary_func!(  "/", |l, r| l.div(r.clone()), data);
+    numeric_binary_func!(  "^", |l, r| l        .pow(r), data);
+    numeric_binary_func!("log", |l, r| l.log(r.clone()), data);
 
-    numeric_unary_func! (   "ln", |n: Num| n   .ln(),      data);
-    numeric_unary_func! ( "log2", |n: Num| n .log2(),      data);
-    numeric_unary_func! ("log10", |n: Num| n.log10(),      data);
-    numeric_unary_func! ( "sqrt", |n: Num| n .sqrt(),      data);
-    numeric_unary_func! (  "exp", |n: Num| n  .exp(),      data);
+    numeric_unary_func! (   "ln", |n: Num| n   .ln(), data);
+    numeric_unary_func! ( "log2", |n: Num| n .log2(), data);
+    numeric_unary_func! ("log10", |n: Num| n.log10(), data);
+    numeric_unary_func! ( "sqrt", |n: Num| n .sqrt(), data);
+    numeric_unary_func! (  "exp", |n: Num| n  .exp(), data);
+    numeric_unary_func! (  "abs", |n: Num| n  .abs(), data);
+
+    numeric_constant_func!("pi", || Num::pi(), data);
+    numeric_constant_func!( "e", || Num:: e(), data);
 
     Env { data, outer: None }
 }
@@ -150,3 +145,4 @@ fn parse_single_number(exp: &Expr) -> Result<Num, Error> {
         _ => Error::reason("Expected a number").into(),
     }
 }
+
